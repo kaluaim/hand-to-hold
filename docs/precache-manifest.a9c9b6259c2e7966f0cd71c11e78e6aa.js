9cbc1f3b311b45526b388c5bac96ceef07d3684d
kaluaim <kaluaim@mit.edu>
2020-04-23 03:28:18 -0400

    Add build

---
+++ docs/precache-manifest.a9c9b6259c2e7966f0cd71c11e78e6aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b39a0bdc3af3f8f1ec99189d55f0ec0",
+    "url": "/hand-to-hold/index.html"
+  },
+  {
+    "revision": "0ba467a9b3095919c0d2",
+    "url": "/hand-to-hold/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5190331ff6564b9bb29a",
+    "url": "/hand-to-hold/static/css/main.e727eb5e.chunk.css"
+  },
+  {
+    "revision": "0ba467a9b3095919c0d2",
+    "url": "/hand-to-hold/static/js/2.4af73825.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hand-to-hold/static/js/2.4af73825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5190331ff6564b9bb29a",
+    "url": "/hand-to-hold/static/js/main.e26e4f93.chunk.js"
+  },
+  {
+    "revision": "d7eadf22b7b35aeb1a7d",
+    "url": "/hand-to-hold/static/js/runtime-main.dd7626fe.js"
+  },
+  {
+    "revision": "d53addffb4d077249c5b1e2e5e091ade",
+    "url": "/hand-to-hold/static/media/dashboard.d53addff.svg"
+  },
+  {
+    "revision": "b3fd9cf3f7caec64c66d62be488858d3",
+    "url": "/hand-to-hold/static/media/logo.b3fd9cf3.svg"
+  },
+  {
+    "revision": "2580d934cff4fc4fd3aa377781eb8487",
+    "url": "/hand-to-hold/static/media/logout.2580d934.svg"
+  },
+  {
+    "revision": "924e05d69e6e31bb3cb16910265cd949",
+    "url": "/hand-to-hold/static/media/my-profile.924e05d6.svg"
+  }
+]);
